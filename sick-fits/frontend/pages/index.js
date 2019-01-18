ae330882d28c501a9c89994824cdbd32f857aed0
adriangm <adrian@secret-source.eu>
2019-01-18 14:18:00 +0000

    Module #1 Set up and Starting with Next

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+// class Home extends React.Component {
+//     render() {
+//         return <p>Hey!</p>
+//     }
+// }
+
+import Link from 'next/link'
+
+const Home = props => (
+    <div>
+        <p>Hey!</p>
+    </div>
+)
+
+export default Home;
